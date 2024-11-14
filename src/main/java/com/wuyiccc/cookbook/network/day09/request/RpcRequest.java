0219af917be5bb820d0c80898042a5d9a7755fa4
wuyiccc <3406324191@qq.com>
2024-11-14 20:06:13 +0800

    feat: hessianSerialize序列化简单使用

---
+++ src/main/java/com/wuyiccc/cookbook/network/day09/request/RpcRequest.java
@@ -0,0 +1,84 @@
+package com.wuyiccc.cookbook.network.day09.request;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+/**
+ * @author wuyiccc
+ * @date 2024/11/14 07:27
+ */
+public class RpcRequest implements Serializable {
+
+    private String requestId;
+
+    /**
+     * 需要调用的接口
+     */
+    private String serviceInterfaceClass;
+
+    /**
+     * 需要调用的方法
+     */
+    private String methodName;
+
+    /**
+     * 方法的参数类型
+     */
+    private Class[] parameterTypes;
+
+    /**
+     * 实际的参数值
+     */
+    private Object[] args;
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getServiceInterfaceClass() {
+        return serviceInterfaceClass;
+    }
+
+    public void setServiceInterfaceClass(String serviceInterfaceClass) {
+        this.serviceInterfaceClass = serviceInterfaceClass;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public Class[] getParameterTypes() {
+        return parameterTypes;
+    }
+
+    public void setParameterTypes(Class[] parameterTypes) {
+        this.parameterTypes = parameterTypes;
+    }
+
+    public Object[] getArgs() {
+        return args;
+    }
+
+    public void setArgs(Object[] args) {
+        this.args = args;
+    }
+
+    @Override
+    public String toString() {
+        return "RpcRequest{" +
+                "requestId='" + requestId + '\'' +
+                ", serviceInterfaceClass='" + serviceInterfaceClass + '\'' +
+                ", methodName='" + methodName + '\'' +
+                ", parameterTypes=" + Arrays.toString(parameterTypes) +
+                ", args=" + Arrays.toString(args) +
+                '}';
+    }
+}
